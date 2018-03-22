56b5c0bcaa47e8c0fd72cfcc9c252b9ba5a9c7db
akhildev <akhildevelop@gmail.com>
2018-03-23 01:03:26 +0530

    initial commit

---
+++ Colorizer/app/tf/demo.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from app.tf.utils import *
+from app.tf.net import Net
+from skimage.io import imsave
+from skimage.transform import resize
+import cv2
+
+def guess():
+
+    img = cv2.imread('static/gray.jpg')
+    if len(img.shape) == 3:
+      img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    img = img[None, :, :, None]
+    data_l = (img.astype(dtype=np.float32)) / 255.0 * 100 - 50
+
+    #data_l = tf.placeholder(tf.float32, shape=(None, None, None, 1))
+    autocolor = Net(train=False)
+
+    conv8_313 = autocolor.inference(data_l)
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+      saver.restore(sess, 'app/tf/models/model.ckpt')
+      conv8_313 = sess.run(conv8_313)
+
+    img_rgb = decode(data_l, conv8_313,2.63)
+    imsave('static/color.jpg', img_rgb)
---
+++ Colorizer/app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+
+from app.tf import demo
+import os
+from django.conf import settings
+from .forms import ImageForm
+
+# Create your views here.
+def index(request):
+    context = {}
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = request.FILES['image']
+            image.name = 'gray.jpg'
+            fullname = os.path.join(settings.MEDIA_ROOT, image.name)
+            if os.path.exists(fullname):
+                os.remove(fullname)
+            fs = FileSystemStorage()
+            filename = fs.save(image.name, image)
+            demo.guess()
+    else:
+        form = ImageForm()
+
+    context['form'] = form
+    return render(request, 'app/index.html', context)
